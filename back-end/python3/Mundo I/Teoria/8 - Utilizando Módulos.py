41e162f59361e884dd5d311a8c659454a95af721
Gustavo Gomes <54083715+gustavocamalionti@users.noreply.github.com>
2022-03-20 00:51:14 -0300

    edit - adicionando módulos e referenciando o terminal

--- back-end/python3/Mundo I/Teoria/8 - Utilizando Módulos.py
+++ back-end/python3/Mundo I/Teoria/8 - Utilizando Módulos.py
@@ -4,6 +4,8 @@ n = int(input('Digite um número: '))
 raiz = math.sqrt(n)
 print(f'A raiz de {n} é {raiz:.3f}. ')
 
+
+
 #Módulo Math Específico
 from math import sqrt
 n = int(input('Digite um número: '))
@@ -16,7 +18,11 @@ print('Computador, me dê um número aleatório entre 0 e 1:',num1)
 
 num2 = random.randint(1,100)
 print('Computador, agora eu quero um número inteiro:',num2)
+#pip list | Esse comando mostra todas as bibliotecas instaladas no pip. Caso a biblioteca emoji não esteja instalada, será necessário importa-la.
 #Importando uma biblioteca da comunidade: Para importar uma biblioteca do pypi, necessita usar no terminal o comando(pip install "E o nome do pacote, sem as aspas")
+#Caso o terminal não esteja identificando a biblioteca instalada, basta mudar o caminho do interprete (CTRL+SHIFT+P >>> Python Select interpreter >>> Enter interpreter path >>> C:\Python310\python.exe)
+import sys
+print(sys.executable)
 
 import emoji
-print(emoji.emojize('Python é legal :thumbsup:', use_aliases = True))
+print(emoji.emojize('Python é legal :thumbsup:', language='alias'))
